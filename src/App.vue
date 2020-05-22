fb7bce48889647e22ff30e5bc5195eed4ac65de9
蔡宏运 <13720275579@163.com>
2020-05-22 10:11:48 +0800

    换主题功能

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,16 @@
 <template>
-  <div id="app">
+  <div id="app" :data-color='dataColor'> 
     <router-view />
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+  data() {
+    return {
+      dataColor:'black',
+    }
+  },
 }
 </script>
